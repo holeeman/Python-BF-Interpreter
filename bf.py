3e7200215d538ae4aed9dbc00babe8918b249a2f
Hosung <runway3207@hanmail.net>
2017-02-17 11:36:24 -0500

    BF Interpreter created

--- bf.py
+++ bf.py
@@ -1,4 +1,5 @@
 import sys
+
 try:
     f = open(sys.argv[1], "r")
 except IndexError:
@@ -7,30 +8,43 @@ except IndexError:
 except IOError:
     print "cannot open file: " + sys.argv[0]
     sys.exit()
-user_input = []
 
 try:
     user_input = [ord(x) for x in sys.argv[2][::-1]]
 except IndexError:
-    pass
+    user_input = []
 
-code = [x for x in f.read()]
+commands = "+-,.[]<>@"
+code = [x for x in f.read() if x in commands]
 space = [0 for x in range(30000)]
 pointer = 0
+loop = []
 stack = []
 string_builder = ""
 
-i = 0
-while i < len(code):
+for i in range(len(code)):
     if code[i] == '[':
         stack.append(i)
+        loop.append(i)
+    if code[i] == ']':
+        loop.insert(loop.index(stack.pop()) + 1, i)
+i = 0
+
+while i < len(code):
+    if code[i] == '[':
+        if space[pointer] == 0:
+            i = loop[loop.index(i) + 1]
     elif code[i] == ']':
         if space[pointer] != 0:
-            i = stack.pop() -1
+            i = loop[loop.index(i) - 1]
     elif code[i] == '+':
         space[pointer] += 1
+        if space[pointer] > 255:
+            space[pointer] = 0
     elif code[i] == '-':
         space[pointer] -= 1
+        if space[pointer] < 0:
+            space[pointer] = 255
     elif code[i] == '.':
         string_builder += chr(space[pointer])
     elif code[i] == ',':
@@ -42,12 +56,13 @@ while i < len(code):
         pointer += 1
     elif code[i] == '<':
         pointer -= 1
-        pointer = max(0, pointer)
 
     elif code[i] == '@':
         print "pointer: " + str(pointer)
-        print "memory:  " + str(space[:10])
-    i +=1
+        print space[:10]
+        print " " + "   "*pointer + "^"
+    # print i, "'"+code[i]+"'", pointer, space[:10]
+    i += 1
 
 if string_builder != "":
     print string_builder
